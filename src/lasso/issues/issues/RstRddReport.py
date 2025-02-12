dcb695c5c8d5b40b23d9c89d7fe45b8e21503801
Catherine Suh <csuh@jpl.nasa.gov>
2025-02-12 13:37:13 -0800

    Fix REGEX for B* (build) label

--- src/lasso/issues/issues/RstRddReport.py
+++ src/lasso/issues/issues/RstRddReport.py
@@ -872,7 +872,7 @@ class NoAcceptanceCriteriaFoundError(Exception):
 class CsvTestCaseReport(RddReport):
     """Report as test cases importable in test management software. Used with testrail."""
 
-    TEST_CASE_REFS_REGEX = ["B[0-9][0-9]\.[0.9]", "s\..*", "p\..*", "requirement", "bug", "i&t.automated"]  # noqa
+    TEST_CASE_REFS_REGEX = ["B[0-9][0-9]\.[0-9]", "s\..*", "p\..*", "requirement", "bug", "i&t.automated"]  # noqa
     # TODO make that an argument
     PROJECT_ID = 168
     # TODO make that an argument
@@ -928,7 +928,7 @@ class CsvTestCaseReport(RddReport):
 
         # print test ref for test coverage
         print(
-            "Kep this list of github ticket references to create the coverage report, using the summary of references:"
+            "Keep this list of github ticket references to create the coverage report, using the summary of references:"
         )
         print("\n".join(self._build_issue_refs))
 
